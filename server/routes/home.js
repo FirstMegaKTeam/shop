f2635d7b57a0229e5e54aae25f7b215d0136993c
Dominik <dominik.kr96@gmail.com>
2021-12-08 23:20:05 +0100

    add get to home routes

--- server/routes/home.js
+++ server/routes/home.js
@@ -3,6 +3,10 @@ const express = require('express');
 const homeRouter = express.Router()
 
 
+homeRouter.get("/", (req,res,next)=>{
+   res.json('siema');
+});
+
 module.exports = {
    homeRouter,
 }
